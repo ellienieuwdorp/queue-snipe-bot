94c16572978dc4e7d08c7cba74e6a8750a8f7f65
Luuk <ellie@nieuwdorp.me>
2020-05-08 22:09:33 +0200

    Implement basic addition and deletion of players & team captains
    TO-DO: figure out a good way to limit players joining to twice the amount of team captains.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const client = new Discord.Client();
 const envy = require('envy');
 const token = envy().token;
 
-const commandPrefix = '!'
+let playerList = [];
+let captainList = [];
+
+const prefix = '!'
 
 client.login(token);
 
@@ -14,8 +17,45 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === commandPrefix + 'ping') {
-        msg.reply('Pong!');
+    if (!msg.content.startsWith(prefix)) return;
+
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'join') {
+        playerList.push(msg.author)
+        msg.reply('you have been added to player pool.')
+    }
+
+    if (command === 'addcaptains') {
+        const mentionedUsers = msg.mentions.users;
+        if (mentionedUsers.size === 0) {
+            msg.reply('please mention the users you want to add as captains after the command. Example: ```!addcaptains @user1 @user2```');
+        }
+        if (mentionedUsers.size > 0) {
+            let usersString = "";
+            mentionedUsers.forEach(function(user) {
+                usersString += (user.username + " ");
+                captainList.push(user);
+            });
+
+            msg.reply('the following users have been added to the list of captains: ' + usersString)
+        }
+    }
+
+    if (command === 'removecaptain') {
+        const indexOfCaptain = captainList.indexOf(msg.mentions.users.first())
+        if (indexOfCaptain === -1) {
+            msg.reply('captain was not removed because they were not on the list of team captains.')
+        }
+        if (indexOfCaptain !== -1) {
+            let removedCaptain = captainList.splice(indexOfCaptain);
+            msg.reply(removedCaptain.username + " has been removed from the list of team captains")
+        }
+    }
+
+    if (command === 'distribute') {
+        // TO-DO
     }
 });
 
