8ee9d1d63e0c5478bc1112b0d79222e4a9bbde8a
Lorenzo Rai <lorenzo@rai-family.ch>
2020-05-10 15:02:39 +0200

    Removed privilege check in addcaptains and removecaptains

--- index.js
+++ index.js
@@ -52,40 +52,29 @@ client.on('message', msg => {
     }
 
     if (command === 'addcaptains') {
-        if(isAuthorizedMessage(msg)) {
+        const mentionedUsers = msg.mentions.users;
+        if (mentionedUsers.size === 0) {
+            msg.reply('please mention the users you want to add as captains after the command. Example: ```!addcaptains @user1 @user2```');
+        }
+        if (mentionedUsers.size > 0) {
+            let usersString = "";
+            mentionedUsers.forEach(function(user) {
+                usersString += (user.username + " ");
+                captainList.push(user);
+            });
 
-            const mentionedUsers = msg.mentions.users;
-            if (mentionedUsers.size === 0) {
-                msg.reply('please mention the users you want to add as captains after the command. Example: ```!addcaptains @user1 @user2```');
-            }
-            if (mentionedUsers.size > 0) {
-                let usersString = "";
-                mentionedUsers.forEach(function(user) {
-                    usersString += (user.username + " ");
-                    captainList.push(user);
-                });
-
-                msg.reply('the following users have been added to the list of captains: ' + usersString)
-            }
-
-        } else {
-            msg.reply('You are not authorized to run this command');
+            msg.reply('the following users have been added to the list of captains: ' + usersString)
         }
     }
 
     if (command === 'removecaptain') {
-        if(isAuthorizedMessage(msg)) {
-            const indexOfCaptain = captainList.indexOf(msg.mentions.users.first())
-            if (indexOfCaptain === -1) {
-                msg.reply('captain was not removed because they were not on the list of team captains.')
-            }
-            if (indexOfCaptain !== -1) {
-                let removedCaptain = captainList.splice(indexOfCaptain);
-                msg.reply(removedCaptain.username + " has been removed from the list of team captains")
-            }
-
-        } else {
-            msg.reply('You are not authorized to run this command');
+        const indexOfCaptain = captainList.indexOf(msg.mentions.users.first())
+        if (indexOfCaptain === -1) {
+            msg.reply('captain was not removed because they were not on the list of team captains.')
+        }
+        if (indexOfCaptain !== -1) {
+            let removedCaptain = captainList.splice(indexOfCaptain);
+            msg.reply(removedCaptain.username + " has been removed from the list of team captains")
         }
     }
 
