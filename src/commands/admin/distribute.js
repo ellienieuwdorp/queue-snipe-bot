545e1d7aea4ffaccad35b2be641f667eeb4228d1
Lorenzo Rai <lorenzo@rai-family.ch>
2021-04-04 18:25:17 +0200

    Fixed tag used in player name

--- src/commands/admin/distribute.js
+++ src/commands/admin/distribute.js
@@ -19,13 +19,13 @@ module.exports = class DistributeCommand extends Command {
 		const teams = queue.mainQueue.distribute(true);
 
 		let index = 1;
-		let response = '';
+		let response = '\n';
 		for(const team of teams) {
 			response = response + 'Team: ' + index + '\n```';
 			index++;
 
 			for(const player of team) {
-				response = response + util.convertIdToTag(player.getDiscordUser().id) + '\n';
+				response = response + util.convertIdToNick(player.getDiscordUser().id) + '\n';
 			}
 
 			response = response + '```\n\n';
