91ed44e8b66e1d32080cc711a20a02b752bcf0be
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2021-05-13 14:55:14 +0200

    Prettier run

--- src/commands/admin/distribute.js
+++ src/commands/admin/distribute.js
@@ -3,34 +3,34 @@ const queue = require("../../queue");
 const util = require("./../../util");
 
 module.exports = class DistributeCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'distribute',
-			aliases: [],
-			group: 'admin',
-			memberName: 'distribute',
-			description: 'Distributed and prints the teams.',
-		});
-	}
-	hasPermission(message) {
-		return util.isAuthorizedMessage(message);
-	}
-	run(message) {
-		const teams = queue.mainQueue.distribute(true);
+    constructor(client) {
+        super(client, {
+            name: "distribute",
+            aliases: [],
+            group: "admin",
+            memberName: "distribute",
+            description: "Distributed and prints the teams.",
+        });
+    }
+    hasPermission(message) {
+        return util.isAuthorizedMessage(message);
+    }
+    run(message) {
+        const teams = queue.mainQueue.distribute(true);
 
-		let index = 1;
-		let response = '\n';
-		for(const team of teams) {
-			response = response + 'Team: ' + index + '\n```';
-			index++;
+        let index = 1;
+        let response = "\n";
+        for (const team of teams) {
+            response = response + "Team: " + index + "\n```";
+            index++;
 
-			for(const player of team) {
-				response = response + util.convertIdToNick(player.getDiscordUser().id) + '\n';
-			}
+            for (const player of team) {
+                response = response + util.convertIdToNick(player.getDiscordUser().id) + "\n";
+            }
 
-			response = response + '```\n\n';
-		}
+            response = response + "```\n\n";
+        }
 
-		message.reply(response);
-	}
+        message.reply(response);
+    }
 };
