4a10e33dac84a22765ae5258fc24c27a6f567c1d
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2021-05-13 14:55:14 +0200

    Prettier run

--- src/queue.js
+++ src/queue.js
@@ -31,161 +31,161 @@ class Player {
 }
 
 class GameQueue {
-	constructor() {
-		// @private
-		this.playerList_ = [];
-	}
+    constructor() {
+        // @private
+        this.playerList_ = [];
+    }
 
-	/**
-	 * Tests if a used is a registered player.
-	 * @param {Discord.User} user
-	 */
-	containsPlayer(user) {
-		for(const player of this.playerList_) {
-			if(player.getDiscordUser() === user) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Tests if a used is a registered player.
+     * @param {Discord.User} user
+     */
+    containsPlayer(user) {
+        for (const player of this.playerList_) {
+            if (player.getDiscordUser() === user) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	findPlayer(user) {
-		for(const player of this.playerList_) {
-			if(player.getDiscordUser() === user) {
-				return player;
-			}
-		}
+    findPlayer(user) {
+        for (const player of this.playerList_) {
+            if (player.getDiscordUser() === user) {
+                return player;
+            }
+        }
 
-		// TODO should this throw an error?
-		return null;
-	}
+        // TODO should this throw an error?
+        return null;
+    }
 
-	addPlayer(user) {
-		if (this.containsPlayer(user)) {
-			return;
-		}
-		this.playerList_.push(new Player(user));
-	}
+    addPlayer(user) {
+        if (this.containsPlayer(user)) {
+            return;
+        }
+        this.playerList_.push(new Player(user));
+    }
 
-	addPlayers(users) {
-		for(const user of users) {
-			this.addPlayer(user);
-		}
-	}
+    addPlayers(users) {
+        for (const user of users) {
+            this.addPlayer(user);
+        }
+    }
 
-	removePlayer(user) {
-		let index = -1;
-		for(let i = 0; i < this.playerList_.length; i++) {
-			if(this.playerList_[i].getDiscordUser() === user) {
-				index = i;
-				break;
-			}
-		}
+    removePlayer(user) {
+        let index = -1;
+        for (let i = 0; i < this.playerList_.length; i++) {
+            if (this.playerList_[i].getDiscordUser() === user) {
+                index = i;
+                break;
+            }
+        }
 
-		if (index === -1) {
-			throw 'Attemped to remove a player from the queue that\'s not in the queue.';
-		}
-		this.playerList_.splice(index, 1);
-	}
+        if (index === -1) {
+            throw "Attemped to remove a player from the queue that's not in the queue.";
+        }
+        this.playerList_.splice(index, 1);
+    }
 
-	removePlayers(users) {
-		for(const user of users) {
-			this.removePlayer(user);
-		}
-	}
+    removePlayers(users) {
+        for (const user of users) {
+            this.removePlayer(user);
+        }
+    }
 
-	setCaptain(user) {
-		this.findPlayer(user).setCaptain();
-	}
+    setCaptain(user) {
+        this.findPlayer(user).setCaptain();
+    }
 
-	setCaptains(users) {
-		for(const user of users) {
-			this.setCaptain(user);
-		}
-	}
+    setCaptains(users) {
+        for (const user of users) {
+            this.setCaptain(user);
+        }
+    }
 
-	unsetCaptain(user) {
-		this.findPlayer(user).unsetCaptain();
-	}
+    unsetCaptain(user) {
+        this.findPlayer(user).unsetCaptain();
+    }
 
-	unsetCaptains(users) {
-		for(const user of users) {
-			this.unsetCaptain(user);
-		}
-	}
+    unsetCaptains(users) {
+        for (const user of users) {
+            this.unsetCaptain(user);
+        }
+    }
 
-	resetQueue() {
-		this.playerList_ = [];
-	}
+    resetQueue() {
+        this.playerList_ = [];
+    }
 
-	getReadableCaptainList() {
-		const s = [];
-		for(const player of this.playerList_) {
-			if(player.isCaptain()) {
-				s.push(util.convertIdToTag(player.getDiscordUser().id));
-			}
-		}
-		return util.concatArray(s);
-	}
+    getReadableCaptainList() {
+        const s = [];
+        for (const player of this.playerList_) {
+            if (player.isCaptain()) {
+                s.push(util.convertIdToTag(player.getDiscordUser().id));
+            }
+        }
+        return util.concatArray(s);
+    }
 
-	getReadableNonCaptainList() {
-		const s = [];
-		for(const player of this.playerList_) {
-			if(!player.isCaptain()) {
-				s.push(util.convertIdToTag(player.getDiscordUser().id));
-			}
-		}
-		return util.concatArray(s);
-	}
+    getReadableNonCaptainList() {
+        const s = [];
+        for (const player of this.playerList_) {
+            if (!player.isCaptain()) {
+                s.push(util.convertIdToTag(player.getDiscordUser().id));
+            }
+        }
+        return util.concatArray(s);
+    }
 
-	/**
-	 * Generates teams and returns them as a list of player lists
-	 *
-	 * @param {Boolean} randomize If true randomizes the player list so that teams will be different
-	 */
-	distribute(randomize) {
-		if(randomize) {
-			util.shuffle(this.playerList_);
-		}
+    /**
+     * Generates teams and returns them as a list of player lists
+     *
+     * @param {Boolean} randomize If true randomizes the player list so that teams will be different
+     */
+    distribute(randomize) {
+        if (randomize) {
+            util.shuffle(this.playerList_);
+        }
 
-		const teams = [];
+        const teams = [];
 
-		// Generate one team for each captain
-		for(const player of this.playerList_) {
-			if(player.isCaptain()) {
-				teams.push([player]);
-			}
-		}
+        // Generate one team for each captain
+        for (const player of this.playerList_) {
+            if (player.isCaptain()) {
+                teams.push([player]);
+            }
+        }
 
-		let currentTeam = 0;
+        let currentTeam = 0;
 
-		// Create more teams if needed
-		if(teams.length * 3 < this.playerList_.length) {
-			// Fill empty teams at the end first
-			currentTeam = teams.length;
+        // Create more teams if needed
+        if (teams.length * 3 < this.playerList_.length) {
+            // Fill empty teams at the end first
+            currentTeam = teams.length;
 
-			const newTeams = Math.ceil(this.playerList_.length / 3.0) - teams.length;
-			for(let i = 0; i < newTeams; i++) {
-				teams.push([]);
-			}
-		}
+            const newTeams = Math.ceil(this.playerList_.length / 3.0) - teams.length;
+            for (let i = 0; i < newTeams; i++) {
+                teams.push([]);
+            }
+        }
 
-		// Distribute non captains to teams
-		for(const player of this.playerList_) {
-			if(!player.isCaptain()) {
-				teams[currentTeam].push(player);
+        // Distribute non captains to teams
+        for (const player of this.playerList_) {
+            if (!player.isCaptain()) {
+                teams[currentTeam].push(player);
 
-				currentTeam++;
-				if(currentTeam >= teams.length) {
-					currentTeam = 0;
-				}
-			}
-		}
+                currentTeam++;
+                if (currentTeam >= teams.length) {
+                    currentTeam = 0;
+                }
+            }
+        }
 
-		console.log(teams);
+        console.log(teams);
 
-		return teams;
-	}
+        return teams;
+    }
 }
 
 module.exports = {
--- src/util.js
+++ src/util.js
@@ -1,39 +1,39 @@
 const { CommandoClient } = require("discord.js-commando");
 
 module.exports = {
-	client: new CommandoClient({
-		commandPrefix: '!',
-		owner: '124993035577720832',
-	}),
-	concatArray: function(arr) {
-		let s = '';
-		arr.forEach(function(v, n) {
-			s = s + v;
-			if (arr.length > n + 1) s = s + ', ';
-		});
-		return s;
-	},
-	// convert id to Discord user mentionable
-	convertIdToTag: function(id) {
-		return this.client.users.cache.get(id).toString();
-	},
-	// convert id to Discord channel nickname
-	convertIdToNick: function(id) {
-		return this.client.users.cache.get(id).tag;
-	},
-	// Returns true if the author of the message is privileged
-	isAuthorizedMessage: function(message) {
-		return (
-			'member' in message &&
-			(message.member.hasPermission('ADMINISTRATOR') ||
-				message.member.roles.cache.find((r) => r.name === 'Queue Snipe Admin'))
-		);
-	},
-	// Shuffles a list using Fisher-Yates
-	shuffle: function(array) {
-		for (let i = array.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[array[i], array[j]] = [array[j], array[i]];
-		}
-	},
+    client: new CommandoClient({
+        commandPrefix: "!",
+        owner: "124993035577720832",
+    }),
+    concatArray: function (arr) {
+        let s = "";
+        arr.forEach(function (v, n) {
+            s = s + v;
+            if (arr.length > n + 1) s = s + ", ";
+        });
+        return s;
+    },
+    // convert id to Discord user mentionable
+    convertIdToTag: function (id) {
+        return this.client.users.cache.get(id).toString();
+    },
+    // convert id to Discord channel nickname
+    convertIdToNick: function (id) {
+        return this.client.users.cache.get(id).tag;
+    },
+    // Returns true if the author of the message is privileged
+    isAuthorizedMessage: function (message) {
+        return (
+            "member" in message &&
+            (message.member.hasPermission("ADMINISTRATOR") ||
+                message.member.roles.cache.find((r) => r.name === "Queue Snipe Admin"))
+        );
+    },
+    // Shuffles a list using Fisher-Yates
+    shuffle: function (array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    },
 };
